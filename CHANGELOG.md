4e8e236715517249d0534be123499037aa2ac2ba
marioIM <mail@mario.im>
2018-06-30 10:06:14 +0200

    code style

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 ## [0.0.1] - 2018-06-25
-
 * Initial release
 
 
 ## [0.0.2] - 2018-06-26
-
 * Remove flutter dependency
+
+
+## [0.0.3] - 2018-06-30
+* code style
--- lib/execution_queue.dart
+++ lib/execution_queue.dart
@@ -2,14 +2,14 @@ library execution_queue;
 
 import 'dart:async';
 
-class Item {
+class _Item {
   final completer;
   final job;
-  Item({this.completer, this.job});
+  _Item(this.completer, this.job);
 }
 
 class ExecutionQueue {
-  List<Item> _queue = [];
+  List<_Item> _queue = [];
   bool _active = false;
 
   void _check() async {
@@ -24,7 +24,7 @@ class ExecutionQueue {
 
   Future<dynamic> add(Function job) {
     var completer = Completer();
-    this._queue.add(Item(completer: completer, job: job));
+    this._queue.add(_Item(completer, job));
     this._check();
     return completer.future;
   }
