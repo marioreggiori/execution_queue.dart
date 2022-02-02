667ba786236c9ab0f283ea6b8bf88a028eebe892
Michael Schwartz <michael@mschwartz.eu>
2022-02-02 19:59:29 +0100

    clear() added, optional timeout added

--- lib/execution_queue.dart
+++ lib/execution_queue.dart
@@ -3,24 +3,38 @@ library execution_queue;
 import 'dart:async';
 
 /// Queue element wrapper
-class _Item {
-  final completer;
-  final job;
-  _Item(this.completer, this.job);
+class _Item<T> {
+  final Completer<T> completer;
+  final Function job;
+
+  const _Item(this.completer, this.job);
 }
 
 /// Queue async jobs to run one after another
 class ExecutionQueue {
-  List<_Item> _queue = [];
+  final List<_Item> _queue = [];
   bool _active = false;
+
+  /// An optional timeout for a job. If the job is not completed in the given
+  /// duration a TimeoutException will be thrown.
+  Duration? timeLimit;
+
+  ExecutionQueue({this.timeLimit});
 
   // check if ready for next job
   void _check() async {
     if (!_active && _queue.isNotEmpty) {
       this._active = true;
-      var item = _queue.removeAt(0);
+      _Item item = _queue.removeAt(0);
       try {
-        var result = await item.job();
+        Future future = item.job();
+        if (timeLimit != null) {
+          future = future.timeout(timeLimit!, onTimeout: () {
+            throw TimeoutException(
+                "Timeout after ${timeLimit.toString()}: ${item.job}");
+          });
+        }
+        var result = await future;
         item.completer.complete(result);
       } catch (e) {
         item.completer.completeError(e);
@@ -30,10 +44,24 @@ class ExecutionQueue {
     }
   }
 
-  /// add async job to queue
+  /// adds async job to queue
   Future<T> add<T>(Function job) {
     var completer = Completer<T>();
-    this._queue.add(_Item(completer, job));
+    this._queue.add(_Item<T>(completer, job));
+    this._check();
+    return completer.future;
+  }
+
+  /// clears the queue as soon as the current job is completed. Returns a
+  /// future which is completed when the clear() is finished.
+  Future clear() {
+    if (_queue.isEmpty) return Future.value();
+    var completer = Completer();
+    this._queue.insert(
+        0,
+        _Item(completer, () {
+          _queue.clear();
+        }));
     this._check();
     return completer.future;
   }
